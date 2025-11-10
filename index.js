4bd2ef8dba4b980664aa8dafd3bb3668bb0cfc2e
markwiz <mmark.kuusik@gmail.com>
2025-11-10 16:57:08 +0200

    put method

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 
-app.use(cors());        // Avoid CORS errors in browsers
-app.use(express.json()) // Populate req.body
+app.use(cors());        
+app.use(express.json()) 
 
 const widgets = [
     { id: 1, name: "Cizzbor", price: 29.99 },
@@ -11,7 +11,11 @@ const widgets = [
     { id: 3, name: "Crazlinger", price: 59.99 },
 ]
 
-// NEW: simple root route to avoid "Cannot GET /"
+const clients = [
+  {id: 1, name: "dildomar", email: "dildomart@suurvarvas.com"},
+  {id: 2, name: "alexander", email: "alexanderthegreate@domination.com"},
+]
+
 app.get('/', (req, res) => {
     res.send({ message: 'Widget API up. Try GET /widgets' });
 });
@@ -37,10 +41,57 @@ app.post('/widgets', (req, res) => {
         name: req.body.name
     }
     widgets.push(newWidget)
-    // FIX: point Location to the new resource by its real id
     res.status(201).location('/widgets/' + newWidget.id).send(newWidget)
 })
 
 app.listen(8080, () => {
-    console.log(`API up at: http://localhost:8080`)
+    console.log(`API up at: http://localhost:8080/widgets`)
+})
+
+app.put('/widgets/:id', (req, res) => {
+    if (typeof widgets[req.params.id - 1] === 'undefined') {
+        return res.status(404).send({ error: "Widget not found" })
+    }
+    widgets[req.params.id - 1].name = req.body.name
+    widgets[req.params.id - 1].price = req.body.price
+    res.send(widgets[req.params.id - 1])
+})
+
+//Client 
+
+app.get('/clients', (req, res) => {
+    res.send(clients)
+})
+
+app.get('/clients/:id', (req, res) => {
+    if (typeof clients[req.params.id - 1] === 'undefined') {
+        return res.status(404).send({ error: "Client not found" })
+    }
+    res.send(clients[req.params.id - 1])
+})
+
+app.post('/clients', (req, res) => {
+    if (!req.body.name || !req.body.email) {
+        return res.status(400).send({ error: 'One or all params are missing' })
+    }
+    let newClient = {
+        id: clients.length + 1,
+        name: req.body.name,
+        email: req.body.email
+    }
+    clients.push(newClient)
+    res.status(201).location('/clients/' + newClient.id).send(newClient)
+})
+
+app.put('/clients/:id', (req, res) => {
+    if (typeof clients[req.params.id - 1] === 'undefined') {
+        return res.status(404).send({ error: "Client not found" })
+    }
+    clients[req.params.id - 1].name = req.body.name
+    clients[req.params.id - 1].email = req.body.email
+    res.send(clients[req.params.id - 1])
+})
+
+app.listen(8080, () => {
+    console.log(`API up at: http://localhost:8080/clients`)
 })
