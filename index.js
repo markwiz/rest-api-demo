3ef12218005a8f445cc5583774aaf81002f4d710
markwiz <mmark.kuusik@gmail.com>
2025-11-17 17:00:58 +0200

    asd

--- index.js
+++ index.js
@@ -16,8 +16,74 @@ const clients = [
   {id: 2, name: "alexander", email: "alexanderthegreate@domination.com"},
 ]
 
+const displayCars = [
+  {
+    Car: {
+      carBrand: "Car 0",
+      color: "Rose red",
+      "Tinted windows": false,
+      Wheels: 4,
+      "Roof cargo": null,
+      Entertainment: [
+        "FM Radio",
+        "MP3, MP4 and MKV player",
+        "harman/kardon speakers"
+      ],
+      Accessories: ["satnav", "cruise control"]
+    }
+  },
+  {
+    Car: {
+      carBrand: "Car1",
+      color: "Navy blue",
+      "Tinted windows": true,
+      Wheels: 4,
+      "Roof cargo": "Thule",
+      Entertainment: [
+        "FM Radio",
+        "Apple CarPlay/Android Auto",
+        "Bowers & Wilkins Premium Sound speakers"
+      ],
+      Accessories: ["self drive system", "luggage cover"]
+    }
+  }
+];
+
+
+app.get('/cars', (req, res) => {
+  res.type('html').send(`
+  <div id="json">
+    <h1> Car Properties </h1>
+
+    <h2>First car</h2>
+    <p>Brand: ${displayCars[0].Car.carBrand}</p>
+    <p>Color: ${displayCars[0].Car.color}</p>
+    <p>Tinted windows: ${displayCars[0].Car["Tinted windows"]}</p>
+    <p>Wheels: ${displayCars[0].Car.Wheels}</p>
+    <p>Roof cargo: ${displayCars[0].Car["Roof cargo"]}</p>
+    <p>Entertainment: ${displayCars[0].Car.Entertainment.join(", ")}</p>
+    <p>Accessories: ${displayCars[0].Car.Accessories.join(", ")}</p>
+
+    <h2>Second car</h2>
+    <p>Brand: ${displayCars[1].Car.carBrand}</p>
+    <p>Color: ${displayCars[1].Car.color}</p>
+    <p>Tinted windows: ${displayCars[1].Car["Tinted windows"]}</p>
+    <p>Wheels: ${displayCars[1].Car.Wheels}</p>
+    <p>Roof cargo: ${displayCars[1].Car["Roof cargo"]}</p>
+    <p>Entertainment: ${displayCars[1].Car.Entertainment.join(", ")}</p>
+    <p>Accessories: ${displayCars[1].Car.Accessories.join(", ")}</p>
+  </div>
+  `);
+});
+// Tagastab JSON kujul kogu autode nimekirja
+app.get('/cars/json', (req, res) => {
+  res.json(displayCars);
+});
+
+
+
 app.get('/', (req, res) => {
-    res.send({ message: 'Widget API up. Try GET /widgets' });
+    res.send({ message: 'Widget API up. Try GET /widgets, /clients, /cars' })
 });
 
 app.get('/widgets', (req, res) => {
@@ -45,7 +111,7 @@ app.post('/widgets', (req, res) => {
 })
 
 app.listen(8080, () => {
-    console.log(`API up at: http://localhost:8080/widgets`)
+    console.log(`API up at: http://localhost:8080 (try /widgets, /clients, /cars)`)
 })
 
 app.put('/widgets/:id', (req, res) => {
@@ -57,7 +123,7 @@ app.put('/widgets/:id', (req, res) => {
     res.send(widgets[req.params.id - 1])
 })
 
-//Client 
+// Client 
 
 app.get('/clients', (req, res) => {
     res.send(clients)
@@ -90,8 +156,4 @@ app.put('/clients/:id', (req, res) => {
     clients[req.params.id - 1].name = req.body.name
     clients[req.params.id - 1].email = req.body.email
     res.send(clients[req.params.id - 1])
-})
-
-app.listen(8080, () => {
-    console.log(`API up at: http://localhost:8080/clients`)
 })
